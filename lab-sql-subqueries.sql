46d8d259a7901a9f2e8ec373ca8a4d79ba4922cf
demonvaa <ernestovaamonde@yahoo.es>
2026-05-04 09:57:28 +0200

    Solucion 04052026 0957

--- lab-sql-subqueries.sql
+++ lab-sql-subqueries.sql
@@ -1,17 +1,9 @@
 -- Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
-USE sakila;
 SELECT 
-title,
-COUNT(title) as num_copias
+    COUNT(i.inventory_id) AS num_copias
 FROM film f
-JOIN inventory i
-	ON f.film_id = i.film_id
-WHERE title = 'Hunchback Impossible'
-  AND EXISTS (
-        SELECT 1
-        FROM inventory i
-        WHERE i.film_id = f.film_id
-  );
+JOIN inventory i ON f.film_id = i.film_id
+WHERE f.title = 'Hunchback Impossible';
   
 -- List all films whose length is longer than the average length of all the films in the Sakila database.
 SELECT title, length
@@ -20,15 +12,16 @@ WHERE length > (SELECT AVG(length) FROM film);
 
 -- Use a subquery to display all actors who appear in the film "Alone Trip".
 SELECT
-first_name,
-last_name
+CONCAT(first_name, ' ', last_name) AS name
 FROM actor a
 WHERE actor_id IN (
     SELECT actor_id
     FROM film_actor fa
-    JOIN film as f
-		ON fa.film_id = f.film_id
-    WHERE title = 'ALONE TRIP'
+    WHERE film_id = (
+        SELECT film_id
+        FROM film
+        WHERE title = 'Alone Trip'
+    )
 );
 
 -- Sales have been lagging among young families, and you want to target family movies for a promotion. 
@@ -46,33 +39,49 @@ WHERE f.film_id IN (
 -- Retrieve the name and email of customers from Canada using both subqueries and joins. 
 -- To use joins, you will need to identify the relevant tables and their primary and foreign keys.
 SELECT 
-cu.first_name,
+    CONCAT(cu.first_name, ' ', cu.last_name) AS name,
+    cu.email
+FROM customer cu
+WHERE cu.address_id IN (
+    SELECT ad.address_id
+    FROM address ad
+    WHERE ad.city_id IN (
+        SELECT ci.city_id
+        FROM city ci
+        WHERE ci.country_id = (
+            SELECT co.country_id
+            FROM country co
+            WHERE co.country = 'Canada'
+        )
+    )
+);
+
+SELECT 
+CONCAT(first_name, ' ', last_name) AS name,
 cu.email 
 FROM customer cu
-
 JOIN address ad 
     ON cu.address_id = ad.address_id
-	WHERE ad.city_id in (
-		SELECT city_id 
-		FROM city ci
-    JOIN country co 
-        ON ci.country_id = co.country_id
-    WHERE co.country = 'Canada'
-);
+JOIN city ci
+	ON ci.city_id = ad.city_id
+JOIN country co
+	 ON ci.country_id = co.country_id
+WHERE co.country = "Canada";
+
+
+
+
+
 
 -- Determine which films were starred by the most prolific actor in the Sakila database.
 --  A prolific actor is defined as the actor who has acted in the most number of films.
 -- First, you will need to find the most prolific actor and then use that actor_id 
 -- to find the different films that he or she starred in.
 SELECT 
-	a.first_name,
 	f.title
 FROM film f
 JOIN film_actor fa 
 	ON f.film_id = fa.film_id
-JOIN actor a
-	ON fa.actor_id = a.actor_id
-
 WHERE fa.actor_id = (
 	SELECT 	actor_id
 	FROM film_actor
@@ -82,13 +91,36 @@ WHERE fa.actor_id = (
 );
 
 
+
 -- Find the films rented by the most profitable customer in the Sakila database. 
 -- You can use the customer and payment tables to find the most profitable customer, i.e.,
 --  the customer who has made the largest sum of payments.
 
-
-
+SELECT title
+FROM film f
+JOIN inventory i
+	ON f.film_id = i.film_id
+WHERE i.inventory_id IN (
+SELECT r.inventory_id
+FROM rental r
+WHERE r.customer_id = (
+    SELECT customer_id
+    FROM payment
+    GROUP BY customer_id
+    ORDER BY SUM(amount) DESC
+    LIMIT 1));
 
 -- Retrieve the client_id and the total_amount_spent of those clients 
 -- who spent more than the average of the total_amount spent by each client.
 -- You can use subqueries to accomplish this.
+WITH totals AS (
+    SELECT 
+        customer_id,
+        SUM(amount) AS total_amount
+    FROM payment
+    GROUP BY customer_id
+)
+SELECT *
+FROM totals
+WHERE total_amount > (SELECT AVG(total_amount) FROM totals)
+ORDER BY total_amount DESC	
